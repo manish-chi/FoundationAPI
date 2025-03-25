f7607141caf8776354a59b4d19826e7b3f3a6f27
manish-chi <chitre.manish@gmail.com>
2025-03-25 10:43:01 +0530

    removed json line as require package

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -8,7 +8,6 @@ exports.checkPrice = (req, res, next) => {
       message: "price not given and name",
     });
   }
-
   next();
 };
 
@@ -29,7 +28,35 @@ exports.createTour = async (req, res) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    let queryObj = { ...req.query };
+
+    console.log(queryObj);
+
+    const excludedFields = ["limit", "sort", "page"];
+
+    excludedFields.forEach((ele) => delete queryObj[ele]);
+
+    let query = Tour.find(queryObj);
+
+    if (req.query.limit) {
+      console.log(req.query.limit);
+      query = query.limit(req.query.limit);
+    }
+
+    if (req.query.sort) {
+      console.log(req.query.sort);
+      query = query.sort(req.query.sort.split(",").join(" "));
+    }
+
+    if (req.query.page) {
+      let page = req.query.page || 1;
+      let limit = req.query.limit || 100;
+      let skip = (page - 1) * limit;
+      query = query.skip(skip).limit(limit).page(page);
+    }
+
+    const tours = await query;
+
     return res.status(200).json({ status: "success", data: tours });
   } catch (err) {
     return res.status(400).json({ status: "fail", message: err.message });
