a1e75bc7a6796bdb00532becb77a8a1f2c004245
manish-chi <chitre.manish@gmail.com>
2025-03-28 10:51:55 +0530

    merged from authentication

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const rateLimit = require("express-rate-limit");
 const tourRouter = require("./Routers/tourRouter");
 const userRouter = require("./Routers/userRouter");
 const globalErrorHandler = require("./Controllers/errorController");
-<<<<<<< Updated upstream
-=======
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
->>>>>>> Stashed changes
 
 dotenv.config({ path: "./config.env" });
 
@@ -21,8 +18,6 @@ app.use(helmet());
 
 app.use(express.json());
 
-<<<<<<< Updated upstream
-=======
 app.use(helmet());
 
 app.use(xss());
@@ -31,13 +26,10 @@ app.use(mongoSanitize());
 
 app.use(hpp());
 
->>>>>>> Stashed changes
 if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"));
 }
 
-<<<<<<< Updated upstream
-=======
 // const limiter = rateLimit({
 //   windowMs: 100,
 //   max: 1,
@@ -46,7 +38,6 @@ if (process.env.NODE_ENV == "development") {
 
 // app.use(limiter);
 
->>>>>>> Stashed changes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
